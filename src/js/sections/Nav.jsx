37060c86e030230ca90cc3b65a5b82eaa4a16f2c
Stanislaw Gregor <gregor.stanislaw@hotmail.com>
2018-06-05 23:13:27 +0200

    Basic Nav functionality

--- src/js/sections/Nav.jsx
+++ src/js/sections/Nav.jsx
@@ -1,10 +1,46 @@
 import React, { Component } from "react";
+import $ from "jquery";
 
 class Nav extends Component {
+  buttonCllickHandler = event => {
+    $(".nav__nav-items").slideToggle();
+  };
+
   render() {
     return (
       <div className="section-wrapper">
-        <section className="nav nav--phone">Nav for phone</section>
+        <section className="nav nav--phone">
+          <div className="nav__wrapper">
+            <span className="nav__logo">
+              Macan<strong>kumbang</strong>
+            </span>
+            <button
+              className="nav__hamburger"
+              onClick={this.buttonCllickHandler}
+            >
+              <i className="fas fa-bars" />
+            </button>
+          </div>
+          <div className="nav__nav-items">
+            <ul>
+              <li className="nav-item">
+                <a href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a href="#">Services</a>
+              </li>
+              <li className="nav-item">
+                <a href="#">Work</a>
+              </li>
+              <li className="nav-item">
+                <a href="#">Testimonials</a>
+              </li>
+              <li className="nav-item">
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </section>
         <section className="nav nav--tablet">Nav for tablet</section>
         <section className="nav nav--desktop">Nav for desktop</section>
       </div>
