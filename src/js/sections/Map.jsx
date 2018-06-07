e25c78d952f855029eb727bca30acacb50eb719d
Stanislaw Gregor <gregor.stanislaw@hotmail.com>
2018-06-07 20:11:27 +0200

    Map prototype

--- src/js/sections/Map.jsx
+++ src/js/sections/Map.jsx
@@ -2,7 +2,18 @@ import React, { Component } from "react";
 
 class Map extends Component {
   render() {
-    return <section className="map">Map</section>;
+    return (
+      <section className="map">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9774.555306898252!2d20.997407669476125!3d52.23177933770987!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ecc8c93a84a83%3A0xa2e1cc1f1b7198de!2sPalace+of+Culture+and+Science%2C+plac+Defilad+1%2C+00-901+Warszawa!5e0!3m2!1sen!2spl!4v1528394943090"
+          width="100%"
+          height="300px"
+          frameBorder="0"
+          style={{ border: "0" }}
+          allowFullScreen
+        />
+      </section>
+    );
   }
 }
 
