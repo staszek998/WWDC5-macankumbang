2569059395644a059960ed29502f5c54797c6c5a
Stanislaw Gregor <gregor.stanislaw@hotmail.com>
2018-06-07 20:44:11 +0200

    Footer prototype

--- src/js/sections/Footer.jsx
+++ src/js/sections/Footer.jsx
@@ -2,7 +2,16 @@ import React, { Component } from "react";
 
 class Footer extends Component {
   render() {
-    return <section className="footer">Footer</section>;
+    return (
+      <section className="footer">
+        <span className="footer__copyright">
+          Copyright &copy; 2017 Macankumbang
+        </span>
+        <span className="footer__logo">
+          Macan<strong>kumbang</strong>
+        </span>
+      </section>
+    );
   }
 }
 
