88ecbe400eaa46cc0b60f371dc103e52224960cf
Stanislaw Gregor <gregor.stanislaw@hotmail.com>
2018-06-09 18:01:43 +0200

    Portfolio section layout for tablet prototype

--- src/js/components/Projects.jsx
+++ src/js/components/Projects.jsx
@@ -3,81 +3,196 @@ import Project from "./Project";
 
 class Projects extends Component {
   render() {
-    if (this.props.which === "web") {
-      return (
-        <div className="projects__wrapper">
-          <Project
-            title="Project for web"
-            description="Project description"
-            href="#"
-          />
-          <Project
-            title="Project for web"
-            description="Project description"
-            href="#"
-          />
-          <Project
-            title="Project for web"
-            description="Project description"
-            href="#"
-          />
-        </div>
-      );
-    } else if (this.props.which === "print") {
-      return (
-        <div className="projects__wrapper">
-          <Project
-            title="Project for print"
-            description="Project description"
-            href="#"
-          />
-          <Project
-            title="Project for print"
-            description="Project description"
-            href="#"
-          />
-          <Project
-            title="Project for print"
-            description="Project description"
-            href="#"
-          />
-        </div>
-      );
-    } else if (this.props.which === "all") {
-      return (
-        <div className="projects__wrapper">
-          <Project
-            title="Project for print"
-            description="Project description"
-            href="#"
-          />
-          <Project
-            title="Project for print"
-            description="Project description"
-            href="#"
-          />
-          <Project
-            title="Project for print"
-            description="Project description"
-            href="#"
-          />
-          <Project
-            title="Project for web"
-            description="Project description"
-            href="#"
-          />
-          <Project
-            title="Project for web"
-            description="Project description"
-            href="#"
-          />
-          <Project
-            title="Project for web"
-            description="Project description"
-            href="#"
-          />
-        </div>
-      );
+    if (this.props.screen === "phone") {
+      if (this.props.which === "web") {
+        return (
+          <div className="projects__wrapper">
+            <Project
+              title="Project for web"
+              description="Project description"
+              href="#"
+            />
+            <Project
+              title="Project for web"
+              description="Project description"
+              href="#"
+            />
+            <Project
+              title="Project for web"
+              description="Project description"
+              href="#"
+            />
+          </div>
+        );
+      } else if (this.props.which === "print") {
+        return (
+          <div className="projects__wrapper">
+            <Project
+              title="Project for print"
+              description="Project description"
+              href="#"
+            />
+            <Project
+              title="Project for print"
+              description="Project description"
+              href="#"
+            />
+            <Project
+              title="Project for print"
+              description="Project description"
+              href="#"
+            />
+          </div>
+        );
+      } else if (this.props.which === "all") {
+        return (
+          <div className="projects__wrapper">
+            <Project
+              title="Project for print"
+              description="Project description"
+              href="#"
+            />
+            <Project
+              title="Project for print"
+              description="Project description"
+              href="#"
+            />
+            <Project
+              title="Project for print"
+              description="Project description"
+              href="#"
+            />
+            <Project
+              title="Project for web"
+              description="Project description"
+              href="#"
+            />
+            <Project
+              title="Project for web"
+              description="Project description"
+              href="#"
+            />
+            <Project
+              title="Project for web"
+              description="Project description"
+              href="#"
+            />
+          </div>
+        );
+      }
+    } else if (this.props.screen === "tablet") {
+      if (this.props.which === "web") {
+        return (
+          <div className="projects__wrapper">
+            <div className="row">
+              <Project
+                title="Project for web"
+                description="Project description"
+                href="#"
+              />
+              <Project
+                title="Project for web"
+                description="Project description"
+                href="#"
+              />
+            </div>
+            <div className="row">
+              <Project
+                title="Project for web"
+                description="Project description"
+                href="#"
+              />
+              <Project
+                title="Project for web"
+                description="Project description"
+                href="#"
+              />
+            </div>
+          </div>
+        );
+      } else if (this.props.which === "print") {
+        return (
+          <div className="projects__wrapper">
+            <div className="row">
+              <Project
+                title="Project for print"
+                description="Project description"
+                href="#"
+              />
+              <Project
+                title="Project for print"
+                description="Project description"
+                href="#"
+              />
+            </div>
+            <div className="row">
+              <Project
+                title="Project for print"
+                description="Project description"
+                href="#"
+              />
+              <Project
+                title="Project for print"
+                description="Project description"
+                href="#"
+              />
+            </div>
+          </div>
+        );
+      } else if (this.props.which === "all") {
+        return (
+          <div className="projects__wrapper">
+            <div className="row">
+              <Project
+                title="Project for print"
+                description="Project description"
+                href="#"
+              />
+              <Project
+                title="Project for print"
+                description="Project description"
+                href="#"
+              />
+            </div>
+            <div className="row">
+              <Project
+                title="Project for print"
+                description="Project description"
+                href="#"
+              />
+              <Project
+                title="Project for web"
+                description="Project description"
+                href="#"
+              />
+            </div>
+            <div className="row">
+              <Project
+                title="Project for web"
+                description="Project description"
+                href="#"
+              />
+              <Project
+                title="Project for web"
+                description="Project description"
+                href="#"
+              />
+            </div>
+            <div className="row">
+              <Project
+                title="Project for print"
+                description="Project description"
+                href="#"
+              />
+              <Project
+                title="Project for web"
+                description="Project description"
+                href="#"
+              />
+            </div>
+          </div>
+        );
+      }
     }
   }
 }
--- src/js/sections/Portfolio.jsx
+++ src/js/sections/Portfolio.jsx
@@ -45,14 +45,46 @@ class Portfolio extends Component {
               Print
             </a>
           </div>
-          <Projects which={this.state.selected} />
+          <Projects which={this.state.selected} screen={"phone"} />
           <span>Browse all</span>
           <a href="#" className="portfolio__link">
             <i className="fas fa-angle-down" />
           </a>
         </section>
         <section className="portfolio portfolio--tablet">
-          Portfolio for tablet
+          <h1 className="portfolio__title">Portfolio</h1>
+          <span className="portfolio__subtitle">Portfolio description</span>
+          <hr />
+          <div className="portfolio__selectors-wrapper">
+            <a
+              href="#"
+              onClick={this.selectorClickHandler}
+              className="portfolio__selector"
+            >
+              All
+            </a>{" "}
+            /{" "}
+            <a
+              href="#"
+              onClick={this.selectorClickHandler}
+              className="portfolio__selector"
+            >
+              Web
+            </a>{" "}
+            /{" "}
+            <a
+              href="#"
+              onClick={this.selectorClickHandler}
+              className="portfolio__selector"
+            >
+              Print
+            </a>
+          </div>
+          <Projects which={this.state.selected} screen={"tablet"} />
+          <span>Browse all</span>
+          <a href="#" className="portfolio__link">
+            <i className="fas fa-angle-down" />
+          </a>
         </section>
         <section className="portfolio portfolio--desktop">
           Portfolio for desktop
