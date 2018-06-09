ddf3f143f3b118a96f3704f7e44b8f2d22063ed1
Stanislaw Gregor <gregor.stanislaw@hotmail.com>
2018-06-09 19:43:44 +0200

    Hero section layout prototype for desktop

--- src/js/sections/Hero.jsx
+++ src/js/sections/Hero.jsx
@@ -21,22 +21,24 @@ class Hero extends Component {
           </a>
         </section>
         <section className="hero hero--tablet-and-desktop">
-          <div className="hero__col">
-            <h1 className="hero__title">Hero title</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-              libero eos aperiam?
-            </p>
-            <a type="button" href="#" className="hero__button">
-              Hero button
-            </a>
-          </div>
-          <div className="hero__col">
-            <img
-              src="http://via.placeholder.com/350x150"
-              alt="Hero image"
-              className="hero__image"
-            />
+          <div className="container">
+            <div className="hero__col">
+              <h1 className="hero__title">Hero title</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+                libero eos aperiam?
+              </p>
+              <a type="button" href="#" className="hero__button">
+                Hero button
+              </a>
+            </div>
+            <div className="hero__col">
+              <img
+                src="http://via.placeholder.com/350x150"
+                alt="Hero image"
+                className="hero__image"
+              />
+            </div>
           </div>
         </section>
       </div>
