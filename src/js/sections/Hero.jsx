5e863a2e7db8e37aaa428e4318df0925e9e6cc70
Stanislaw Gregor <gregor.stanislaw@hotmail.com>
2018-06-10 10:50:55 +0200

    Hero section - working layout for phone

--- src/js/sections/Hero.jsx
+++ src/js/sections/Hero.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-// import $ from "jquery";
+import image from "../../images/header/image.png";
 
 class Hero extends Component {
   render() {
@@ -7,18 +7,14 @@ class Hero extends Component {
       <div className="section-wrapper">
         <section className="hero hero--phone">
           <h1 className="hero__title">Hero title</h1>
-          <p>
+          <p className="hero__subtitle">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
             libero eos aperiam?
           </p>
-          <img
-            src="http://via.placeholder.com/350x150"
-            alt="Hero image"
-            className="hero__image"
-          />
-          <a type="button" href="#" className="hero__button">
-            Hero button
-          </a>
+          <img src={image} alt="Hero image" className="hero__image" />
+          <button type="button" href="#" className="hero__button">
+            More
+          </button>
         </section>
         <section className="hero hero--tablet-and-desktop">
           <div className="container">
