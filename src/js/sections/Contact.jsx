755e603918097795973fd5fba5552a599f70323d
Stanislaw Gregor <gregor.stanislaw@hotmail.com>
2018-06-13 19:10:48 +0200

    Contact section - working layout for desktop

--- src/js/sections/Contact.jsx
+++ src/js/sections/Contact.jsx
@@ -42,18 +42,32 @@ class Contact extends Component {
         </section>
         <section className="contact--desktop">
           <div className="container">
-            <h2 className="contact__title">Contact title</h2>
-            <span className="contact__subtitle">Contact subtitle</span>
-            <hr />
+            <h2 className="section-title contact__title">Contact</h2>
+            <span className="section-subtitle contact__subtitle">
+              Let's talk business
+            </span>
+            <img
+              src={separator}
+              className="contact__separator"
+              alt="Separator"
+            />
             <div className="contact__wrapper">
               <div className="contact__contact-details">
                 <span>Office Hours:</span>
                 <span>Monday to Friday 07:00-17:00</span>
                 <span>Saturday 09:00-15:00</span>
-                <br />
-                <span>Plac Defilad 1, 00-901 Warsaw</span>
-                <span>+48 667 882 721</span>
-                <span>stanislaw.gregor@outlook.com</span>
+                <div className="row">
+                  <i className="fas fa-map-marker" />
+                  <span>Plac Defilad 1, 00-901 Warsaw</span>
+                </div>
+                <div className="row">
+                  <i className="fas fa-phone" />
+                  <span>+48 667 882 721</span>
+                </div>
+                <div className="row">
+                  <i className="fas fa-envelope" />
+                  <span>stanislaw.gregor@outlook.com</span>
+                </div>
                 <div className="social-media-icons">
                   <div className="icon">
                     <i className="fab fa-facebook-f" />
